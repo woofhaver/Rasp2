7c0f6907bdb7106d2337fd406a74bb9d1a6c1746
Woof Haver <woof.haver@gmail.com>
2015-12-04 17:44:11 +0100

    Init3

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raspberry.IO.GeneralPurpose;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redLed = ConnectorPin.P1Pin07.Output();
+            var gpio = new GpioConnection(redLed);
+            gpio.Toggle(redLed);
+
+            Thread.Sleep(10000);
+            gpio.Close();
+        }
+    }
+}
